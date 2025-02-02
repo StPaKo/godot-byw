07474239d1548c851a3be67e7be6b1dc78ada802
StPaKo <s.pavlov.boxl@gmail.com>
2025-02-02 21:21:11 +0300

    Little optimization hotfix in weapons shooter script

--- scripts/weapons/weapon.gd
+++ scripts/weapons/weapon.gd
@@ -2,6 +2,7 @@ extends RayCast2D
 
 var lock_rotation: bool = false
 var looked_object_position: Vector2 = Vector2(0, 0)
+@export var shooter: ShootComponent
 @export var bullet_type: PackedScene
 @export var bullet_speed: float = 1000
 @export var bullets_on_shot: int = 1
@@ -21,9 +22,4 @@ func set_rotation_lock(is_locked: bool, look_at_position: Vector2 = Vector2(0, 0
 		self.looked_object_position = look_at_position
 
 func _on_force_shot(delta: float) -> void:
-	for child in self.get_children():
-		if child.name == "Components":
-			for component in child.get_children():
-				if component is ShootComponent:
-					component.shoot(delta)
-					break
+	shooter.shoot(delta)
