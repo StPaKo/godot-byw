f7412066e4fb1c95735338f70bebef0aceb60eed
StPaKo <s.pavlov.boxl@gmail.com>
2025-02-03 22:14:51 +0300

    Added drag-and-drop inventory system Added moveable Double Bullet modification TO-DO: 1. Blank inventory spaces 2. Influence of inventory content on weapons

--- scripts/components/weapons/InventoryComponent.gd
+++ scripts/components/weapons/InventoryComponent.gd
@@ -6,10 +6,11 @@ var inventory = []
 @export var column_amount : int = 2
 @export var generator_location: int = 0
 @export var receiver_location: int = 1
-@onready var background = $Panel
-@onready var container = $Panel/GridContainer
+@onready var background : Panel = $Panel
+@onready var container : GridContainer = $Panel/GridContainer
 const inventory_slot_preload : PackedScene = preload("res://objects/ui/inventory/inventoryslot.tscn")
 const slot_offset : int = 5
+var dragged_slot : InventorySlot = null
 
 func _ready() -> void:
 	inventory.resize(inventory_size)
@@ -27,6 +28,14 @@ func _ready() -> void:
 		"Receiver",
 		"Consumes energy so your weapon can fire"
 	)
+	
+	# remove later
+	inventory[0] = Modification.new(
+		ImageTexture.create_from_image(Image.load_from_file("res://assets/ui/modifications/double_bullet 16x16.png")),
+		"Double bullet",
+		"Doubles the amount of fired bullets"
+	)
+	
 	_on_inventory_updated()
 
 func _process(delta: float) -> void:
@@ -46,6 +55,8 @@ func _on_inventory_updated() -> void:
 	container.columns = column_amount
 	for item in self.inventory:
 		var slot = inventory_slot_preload.instantiate()
+		slot.slot_dragged.connect(_on_slot_dragged)
+		slot.slot_dropped.connect(_on_slot_dropped)
 		container.add_child(slot)
 		if item != null:
 			slot.set_modification(item.get_item())
@@ -57,3 +68,38 @@ func clear() -> void:
 		var child = container.get_child(0)
 		container.remove_child(child)
 		child.queue_free()
+
+func _on_slot_dragged(slot: InventorySlot) -> void:
+	dragged_slot = slot
+
+func _on_slot_dropped() -> void:
+	var drop_target : InventorySlot = get_slot_under_mouse()
+	if drop_target and dragged_slot != drop_target:
+		drop_slot(dragged_slot, drop_target)
+	dragged_slot = null
+
+func get_slot_under_mouse() -> InventorySlot:
+	var mouse_position : Vector2 = get_global_mouse_position()
+	for slot in container.get_children():
+		var slot_rect = Rect2(slot.global_position, slot.size)
+		if slot_rect.has_point(mouse_position):
+			return slot
+	return null
+
+func get_slot_index(slot: InventorySlot) -> int:
+	for i in range(container.get_child_count()):
+		if container.get_child(i) == slot:
+			return i
+	return -1
+
+func drop_slot(drag_slot: InventorySlot, drop_slot: InventorySlot) -> void:
+	var drag_slot_idx : int = get_slot_index(drag_slot)
+	var drop_slot_idx : int = get_slot_index(drop_slot)
+	if drag_slot_idx < 0 or drag_slot_idx > inventory.size() or drop_slot_idx < 0 or drop_slot_idx > inventory.size():
+		return
+	if drop_slot.contained_modification["name"] in ["Generator", "Receiver"]:
+		return
+	var temp : Modification = inventory[drop_slot_idx]
+	inventory[drop_slot_idx] = inventory[drag_slot_idx]
+	inventory[drag_slot_idx] = temp
+	_on_inventory_updated()
--- scripts/ui/inventory/inventoryslot.gd
+++ scripts/ui/inventory/inventoryslot.gd
@@ -5,10 +5,18 @@ class_name InventorySlot
 @onready var modification_icon : Sprite2D = $InnerBorder/ItemIcon
 @onready var modification_name_label : Label = $InfoPanel/ItemName
 @onready var modification_description_label : Label = $InfoPanel/Description
-var contained_modification = null
+@onready var outer_border : ColorRect = $OuterBorder
+var contained_modification : Dictionary = {
+	"texture": null,
+	"name": null,
+	"description": null
+}
+
+signal slot_dragged(slot: InventorySlot)
+signal slot_dropped
 
 func clear() -> void:
-	modification_icon = null
+	contained_modification = { "texture": null, "name": null, "description": null }
 
 func set_modification(new_modification : Dictionary) -> void:
 	self.contained_modification = new_modification
@@ -17,8 +25,19 @@ func set_modification(new_modification : Dictionary) -> void:
 	self.modification_description_label.text = new_modification["description"]
 
 func _on_mouse_entered() -> void:
-	if contained_modification != null:
+	if contained_modification != { "texture": null, "name": null, "description": null }:
 		info_panel.visible = true
 
 func _on_mouse_exited() -> void:
 	info_panel.visible = false
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.is_pressed():
+				outer_border.modulate = Color(0, 0, 0)
+				if not contained_modification["name"] in ["Generator", "Receiver"]:
+					slot_dragged.emit(self)
+			else:
+				outer_border.modulate = Color(1, 1, 1)
+				slot_dropped.emit()
